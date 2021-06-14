ed4ddcea3c5eb8efbd52f5b5d22ea365aa351b79
mroyon@gmail.com <mroyon@gmail.com>
2021-06-14 11:47:28 +0300

    first time

---
+++ Controllers/MyFirstController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyFirstMVCWebApp.Controllers
+{
+    public class MyFirstController : Controller
+    {
+
+        // default view 
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        // MySecondView view 
+        public IActionResult MySecondView()
+        {
+            return View();
+        }
+    }
+}
